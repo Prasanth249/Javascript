05d69ef4437fb919fa100a77c40154c6186e8e2b
sakenikitha123 <sakenikitha123@gmail.com>
2023-11-07 09:26:09 +0530

    Javascript

---
+++ 43.Pure_Impure/pure_impureFunction.js
@@ -0,0 +1,18 @@
+// let x=[10,20,30]
+// function pure(arr,num){
+//     arr.push(num)
+// }
+// console.log(x)
+// pure(x,5)
+// console.log(x)
+
+let x=[10,20,30]
+function pure(arr,num){
+    let newArr=[...arr,num]
+    return newArr;
+}
+console.log(x)
+console.log(pure(x,5))
+console.log(x)
+
+
